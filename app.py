b5d6914dc7c36b494304f0fc63bb638930c2ae5e
Rajiv <admin@admins-MacBook-Air.local>
2025-03-29 15:20:55 -0700

    Streamlit app with MySQL DB

---
+++ app.py
@@ -0,0 +1,99 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import mysql.connector
+
+def connectToDB():
+    return mysql.connector.connect(
+        host = '127.0.0.1',
+        user = 'root',
+        password = 'password',
+        database = 'resort'
+    )
+
+def register_user(email, password):
+    conn = connectToDB()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM USER WHERE name = %s", (email,))
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        st.error("Email already registered. Please log in.")
+    else:
+        cursor.execute("INSERT INTO USER (name, password) VALUES (%s, %s)", (email, password))
+        conn.commit()
+        st.success("Registration successful! You can now log in.")
+
+    cursor.close()
+    conn.close()
+
+def login_user(email, password):
+    conn = connectToDB()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM USER WHERE name =  %s AND password = %s", (email, password))
+    existing_user = cursor.fetchone()
+    if existing_user:
+        st.success("Login Successful")
+    else:
+        st.error("Email or Password incorrect. Try Again.")
+    
+    cursor.close()
+    conn.close()
+
+#UI frontend code 
+
+st.markdown(
+    """
+    <style>
+    .title {
+        position: relative;
+        top: -70px;  /* Adjust this value to move title higher */
+        text-align: Left;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown('<h1 class="title">ReSort</h1>', unsafe_allow_html=True)
+
+selected = option_menu(
+    menu_title = None,
+    options = ["Home", "Login", "Register"],
+    icons = ["house", "key-fill", "key"],
+    menu_icon = "list",
+    orientation = "horizontal",
+)
+
+placeholder = st.empty()
+
+if selected == "Home":
+    st.subheader("Revolutionizing Waste Sorting")
+    st.image("https://media.istockphoto.com/id/962933762/vector/ecology-and-waste-global-eco-friendly-plastic.jpg?s=612x612&w=0&k=20&c=RdbOw__qI_Vc0W8pU0dEiO9--Unfs-iXUEqQOCP-1HE=", width = 400)
+if selected == "Login":
+    with placeholder.form("login"):
+        st.markdown("#### Login")
+        email = st.text_input("Email")
+        password = st.text_input("Password", type="password")
+        submit = st.form_submit_button("Login")
+        if submit:
+            if email and password:
+                login_user(email, password)
+            else:
+                st.error("Please make sure you fill out all fields")
+
+if selected == "Register":
+    with placeholder.form("Register"):
+        st.markdown("#### Register")
+        email = st.text_input("Email")
+        password = st.text_input("Password", type="password")
+        submit = st.form_submit_button("Register")
+        if submit:
+            if email and password:
+                register_user(email, password)
+            else:
+                st.error("Please make sure you fill out all fields")
+
+
+
